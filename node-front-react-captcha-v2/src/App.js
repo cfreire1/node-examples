4d96b30afc102cb88b2cd914afea338c9c3fba58
cristopher freire <christ.freire@hotmail.com>
2022-12-25 18:20:47 -0300

    feat: acttualizacion de codigo y comentarios.

--- node-front-react-captcha-v2/src/App.js
+++ node-front-react-captcha-v2/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>Prueba de formulario simple con ReCAPTCHA v2</p>
         <FormComponent />
       </header>
--- node-front-react-captcha-v2/src/components/FormComponent.js
+++ node-front-react-captcha-v2/src/components/FormComponent.js
@@ -1,76 +1,85 @@
 import React, { useRef } from 'react'
 import ReCAPTCHA from "react-google-recaptcha"
 
-
+/**
+ * Componente que muestra formulario con ReCAPTCHA v2
+ * @returns Formulario con ReCAPTCHA
+ */
 export const FormComponent = () => {
 
+  /**
+   * Variable para Manejar el comportamiendo del ReCAPTCHA
+   */
   let validatecatcha = false;
+
+  /**
+   * Variable para restablecer el reCAPTCHA
+   */
   const captchaRef = useRef(null)
 
+
+  /**
+   * Metodo que valida que ReCAPTCHA este realizado.
+   */
   const validateCatcha = () => {
     if (!validatecatcha) {
       alert('Favor validar ReCAPTCHA!!');
-    }else{
+    } else {
       alert('Enviado!!');
     }
   }
 
+  /**
+   * Metodo que cambia el estado ReCAPTCHA cuando este a sido realizado.
+   */
   const checkCatcha = () => {
-    validatecatcha=true;
+    validatecatcha = true;
     console.log(validatecatcha);
   }
 
 
   /**
-   * Metodo para generar token de sitio y enviarlo a validar al backend
+   * Metodo para generar token de sitio y es enviado a validar al backend
    * @param {*} e 
    */
-  const handleSubmit = async (e) =>{
-    e.preventDefault();
-    const token = captchaRef.current.getValue();//token a verificar con back-end
-    captchaRef.current.reset();
+  const handleSubmit = (e) => {
+    e.preventDefault();//Quita el comportamiento por defecto(submit). (para que no refresque la pagina)
+    const token = captchaRef.current.getValue();//token generado de nuestro reCAPTCHA a verificar con back-end
+    captchaRef.current.reset();//Después de cada verificación, debemos restablecer el reCAPTCHA
     console.log(token);
 
     //===========================seteo de datos a objeto
 
-    //envio a validar(rest) al backend
-    const res = await fetch(process.env.REACT_APP_API_URL,{
-      method:'POST',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({'token': token})
-    }); 
+    //envio - validar al backend
+    fetch(process.env.REACT_APP_API_URL, {//envio de datos a api
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 'token': token })
+    })
+      .then(response => response.json())//convirtiendo la respuesta en JSON
+      .then(data => console.log(data))//respuesta del back-end
+      .catch(err => console.log(err));//Por si hay algun error
+  }
 
-
-    //respuesta del back-end
-    const resData = res.json();
-    resData.then((result) => {
-
-      //exito
-      console.log(result);
-
-    }).catch((err) => {
-      console.log(err);
-    });
-
-}
-  
+  //retorno
   return (
     <div>
       <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
       <form onSubmit={handleSubmit} >
-            <label htmlFor="name">Name</label>
-                <input type="text" id="name" className="input"/>
-            <button onClick={validateCatcha} >Submit</button>
 
-            <ReCAPTCHA 
-                  ref={captchaRef}
-                  sitekey={process.env.REACT_APP_SITE_KEY}
-                  render="explicit"
-                  onChange={checkCatcha}
-                />
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" className="input" />
+        <button onClick={validateCatcha} >Submit</button>
+
+        <ReCAPTCHA
+          ref={captchaRef}
+          sitekey={process.env.REACT_APP_SITE_KEY}
+          render="explicit"
+          onChange={checkCatcha}
+        />
       </form>
     </div>
-    
+
   )
 }
